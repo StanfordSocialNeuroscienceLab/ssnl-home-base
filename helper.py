afeaa4a875a19d8918a8040ba9053308887c86ea
Ian <47256454+IanRFerguson@users.noreply.github.com>
2022-06-01 20:52:09 -0700

    Create helper.py

---
+++ helper.py
@@ -0,0 +1,30 @@
+# --- Imports
+from flask import Flask, render_template, url_for, redirect, request
+import json, sqlite3, sys, os, pathlib, shutil
+from datetime import datetime
+
+from pexpect import ExceptionPexpect
+
+
+# --- Helpers
+def cleanup_output(path):
+      """
+      Quick function to wipe out any previously uploaded files at init
+      """
+
+      for file in os.listdir(path):
+            if file != ".DS_Store":
+                  
+                  temp = os.path.join(path, file)
+
+                  if os.path.isdir(temp):
+                        try:
+                              shutil.rmtree(temp)
+                        except Exception as e:
+                              print(e)
+
+                  elif os.path.isfile(temp):
+                        try:
+                              os.remove(temp)
+                        except Exception as e:
+                              print(e)
