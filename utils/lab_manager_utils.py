72f221df464a3f26e3e6a87c8df26f836b163c33
Ian <47256454+IanRFerguson@users.noreply.github.com>
2022-07-04 19:56:22 -0700

    Update lab_manager_utils.py

--- utils/lab_manager_utils.py
+++ utils/lab_manager_utils.py
@@ -2,6 +2,7 @@
 
 # --- Imports
 import os, json
+from sre_constants import SRE_FLAG_TEMPLATE
 import pandas as pd
 from main import path_to_members, path_to_projects
 
@@ -73,3 +74,100 @@ def build_projects_df():
             projects_.append(clean_dictionary)
 
       return output.reset_index(drop=True), projects_
+
+
+class JSONCursor:
+
+      def __init__(self, path_to_packets, packet="members"):
+            """
+            Simple wrapper to perform input and output functions
+            on the underlying members and projects dictionaries
+            """
+
+            self.base_path = path_to_packets
+            self.packet_type = packet
+
+            self.filepath = os.path.join(self.base_path)
+
+
+      def open_file(self):
+
+            with open(self.filepath) as incoming:
+                  return json.load(incoming)
+
+
+      def save_file(self, temp_dictionary):
+
+            with open(self.filepath, "w") as outgoing:
+                  json.dump(temp_dictionary, outgoing, indent=4)
+
+
+
+# -- Update
+class MembersCursor(JSONCursor):
+
+      def __init__(self, path_to_packets, key, employee_number,
+                   title, create_new_key=False):
+            
+            JSONCursor.__init__(self, path_to_packets, packet="members")
+
+            self.key = key
+            self.employee_number = employee_number
+            self.title = title
+
+            self.create_new_key = create_new_key
+
+      
+      def run(self):
+
+            temp_data = self.open_file()
+
+
+            if self.create_new_key:
+                  temp_data[self.key] = {}
+
+
+            if self.employee_number != "":
+                  temp_data[self.key]["employee_number"] = self.employee_number
+
+            if self.title != "":
+                  temp_data[self.key]["title"] = self.title
+
+            self.save_file(temp_dictionary=temp_data)
+
+
+
+class ProjectsCursor(JSONCursor):
+
+      def __init__(self, path_to_packets, key, pta, sponsor,
+                   irb_number, create_new_key=False):
+            
+            JSONCursor.__init__(self, path_to_packets=path_to_packets, packet="projects")
+
+            self.key = key
+            self.pta = pta
+            self.sponsor = sponsor
+            self.irb_number = irb_number
+
+            self.create_new_key = create_new_key
+
+
+      def run(self):
+
+            temp_data = self.open_file()
+
+
+            if self.create_new_key:
+                  temp_data[self.key] = {}
+
+
+            if self.pta != "":
+                  temp_data[self.key]["pta"] = self.pta
+
+            if self.sponsor != "":
+                  temp_data[self.key]["sponsor"] = self.sponsor
+
+            if self.irb_number != "":
+                  temp_data[self.key]["irb_number"] = self.irb_number
+
+            self.save_file(temp_dictionary=temp_data)
