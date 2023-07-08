6711f153154d2adfde13180f856623c477262c2f
Ian <47256454+IanRFerguson@users.noreply.github.com>
2023-07-08 17:02:38 -0400

    add dev functionality

--- config.py
+++ config.py
@@ -5,14 +5,17 @@ import pytz
 
 
 class SSNLConfig:
-    HERE = os.path.abspath(os.path.dirname(__file__))
+    DEVMODE = True
+
+    ROOT = os.path.abspath(os.path.dirname(__file__))
+    HERE = os.path.join(ROOT, "ssnl")
 
     SECRET_KEY = "jamil4ever"
     UPLOAD_FOLDER = "files/uploads"
     JUSTIFICATIONS = "files/justifications"
     BP_TEMPLATES = "files/templates"
 
-    JSON_PATH = "files/packets"
+    JSON_PATH = os.path.join(HERE, "files/packets")
     MEMBER_PATH = os.path.join(JSON_PATH, "members.json")
     PROJECT_PATH = os.path.join(JSON_PATH, "projects.json")
     ADMIN_PATH = os.path.join(JSON_PATH, "admin.json")
