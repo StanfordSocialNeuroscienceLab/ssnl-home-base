b38c7a39c6b23987c9ef3fdd25b69ed7d63820f0
Ian <47256454+IanRFerguson@users.noreply.github.com>
2023-08-17 21:46:23 -0400

    teardown + revamp

--- ssnl/admin/routes.py
+++ ssnl/admin/routes.py
@@ -1,8 +1,9 @@
 ### LAB MANAGER / ADMIN ROUTES ###
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 from flask_httpauth import HTTPBasicAuth
 from ssnl.admin import bp
 from config import SSNLConfig
+from ssnl.common.utils import get_members, get_projects, get_reocurring_projects
 
 ##########
 
@@ -30,46 +31,64 @@ def index():
 @bp.route("/landed", methods=methods)
 @auth.login_required
 def landing():
-    return render_template("admin/landing.html", manager_name=None)
+    endpoint_map = {
+        "View Members": "admin.view_members",
+        "View Finance Sources": "admin.view_projects",
+        "View Recurring Projects": "admin.view_recurring",
+        "Update Members": "admin.update_members",
+        "Update Finance Sources": "admin.update_projects",
+        "Update Recurring Projects": "admin.update_recurring",
+        "Add Member": "admin.add_member",
+        "Add Finance Source": "admin.add_project",
+        "Add Recurring Projects": "admin.add_recurring",
+    }
+
+    if request.method == "POST":
+        endpoint = request.form["endpoint"]
+        return redirect(url_for(endpoint=endpoint))
+
+    return render_template(
+        "admin/landing.html", manager_name=None, endpoint_map=endpoint_map
+    )
 
 
 @bp.route("/view_members", methods=methods)
 @auth.login_required
 def view_members():
     member_data = None
-    return render_template("admin/members.html", data=member_data)
+    return render_template("admin/member__view.html", data=member_data)
 
 
 @bp.route("/view_projects", methods=methods)
 @auth.login_required
 def view_projects():
     member_data = None
-    return render_template("admin/members.html", data=member_data)
+    return render_template("admin/project__view.html", data=member_data)
 
 
 @bp.route("/update_members", methods=methods)
 @auth.login_required
 def update_members():
     member_data = None
-    return render_template("admin/members.html", data=member_data)
+    return render_template("admin/member__update.html", data=member_data)
 
 
 @bp.route("/update_projects", methods=methods)
 @auth.login_required
 def update_projects():
     member_data = None
-    return render_template("admin/members.html", data=member_data)
+    return render_template("admin/project__update.html", data=member_data)
 
 
 @bp.route("/add_member", methods=methods)
 @auth.login_required
 def add_member():
     member_data = None
-    return render_template("admin/members.html", data=member_data)
+    return render_template("admin/member__add.html", data=member_data)
 
 
 @bp.route("/add_project", methods=methods)
 @auth.login_required
 def add_project():
     member_data = None
-    return render_template("admin/members.html", data=member_data)
+    return render_template("admin/project__add.html", data=member_data)
